2d3bc379f9df6074f1d805ea016129e55bec9e7a
Adek Maulana <adek@techdro.id>
2020-12-29 17:10:49 +0700

    caligo: add logging
    Change-Id: Icb0a3c0791e7214927e81ba37b0aca0c099b16cc

---
+++ caligo/logging.py
@@ -0,0 +1,35 @@
+import logging
+import colorlog
+
+from datetime import datetime
+
+
+def setup_log() -> None:
+    """Configures logging"""
+    level = logging.INFO
+
+    logging.root.setLevel(level)
+
+    # Logging into file
+    format = "[ %(asctime)s : %(levelname)-7s ] %(name)s | %(message)s"
+    logfile_name = f"caligo-{datetime.now().strftime('%Y-%m-%d')}.log"
+    logfile = logging.FileHandler(f"caligo/{logfile_name}")
+    formatter = logging.Formatter(format, datefmt="%H:%M:%S")
+    logfile.setFormatter(formatter)
+    logfile.setLevel(level)
+
+    # Logging into stdout with color
+    format = ("  %(log_color)s%(levelname)s%(reset)s  |  %(name)s  |  "
+              "%(log_color)s%(message)s%(reset)s")
+    stream = logging.StreamHandler()
+    formatter = colorlog.ColoredFormatter(format)
+    stream.setLevel(level)
+    stream.setFormatter(formatter)
+
+    root = logging.getLogger()
+    root.setLevel(level)
+    root.addHandler(logfile)
+    root.addHandler(stream)
+
+    # only set level to WARNING
+    logging.getLogger("pyrogram").setLevel(logging.WARNING)

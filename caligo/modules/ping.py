41ac12aa798bb1065aa509be6cf3590c439eed5b
Adek Maulana <adek@techdro.id>
2021-02-25 05:58:53 +0700

    patch: 5; add ping
    Change-Id: If417d1c8b89a3ee9035590558da28a8fd65537f7

---
+++ caligo/modules/ping.py
@@ -0,0 +1,17 @@
+from typing import ClassVar
+from datetime import datetime
+
+from .. import command, module
+
+
+class Ping(module.Module):
+    name: ClassVar[str] = "Ping"
+
+    @command.desc("Pong")
+    async def cmd_ping(self, ctx: command.Context):
+        start = datetime.now()
+        await ctx.respond("Calculating response time...")
+        end = datetime.now()
+        latency = (end - start).microseconds / 1000
+
+        return f"Request response time: **{latency} ms**"

8b13f3f5dfa23743283da85d4f39252644d0afb3
Adek Maulana <adek@techdro.id>
2021-03-13 11:25:20 +0700

    aria2: update, improve and re-enable
    Change-Id: I931e3b89e86e9f0cc5e2f4321bcd9692a56b5893

--- caligo/modules/aria2.py
+++ caligo/modules/aria2.py
@@ -1,3 +1,4 @@
+import asyncio
 from pathlib import Path
 from typing import Any, ClassVar, Dict, Union
 
@@ -34,7 +35,7 @@ class Aria2WebSocket:
             "--rpc-max-request-size=1024M",
             "--seed-ratio=1",
             "--seed-time=60",
-            "--max-upload-limit=5K",
+            "--max-upload-limit=1024K",
             "--max-concurrent-downloads=5",
             "--min-split-size=10M",
             "--follow-torrent=mem",
@@ -54,35 +55,79 @@ class Aria2WebSocket:
             url="http://localhost:8080/jsonrpc"
         )
 
-        trigger_names = ["Start", "Pause", "Stop", "Complete", "Error"]
+        trigger_names = ["Start", "Complete", "Error"]
         for handler_name in trigger_names:
             client.register(self.on_trigger, f"aria2.onDownload{handler_name}")
         return client
 
     async def on_trigger(
         self,
-        trigger: aioaria2.Aria2WebsocketTrigger,  # skipcq: PYL-W0613
+        trigger: aioaria2.Aria2WebsocketTrigger,
         data: Union[Dict[str, str], Any]
     ):
         method = data.get("method").removeprefix("aria2.")
+        gid = data["params"][0]["gid"]
+
+        if method == "onDownloadComplete":
+            cache = await trigger.tellStatus(gid, ["followedBy"])
+            res = self.mod.data[gid]
+            if cache:
+                res.put_nowait((cache["followedBy"][0], True))
+            else:
+                res.put_nowait((None, False))
 
         update = getattr(self.mod, method)
-        await update(data.get("params")[0]["gid"])
+        await update(gid)
 
 
 class Aria2(module.Module):
     name: ClassVar[str] = "Aria2"
-    disabled: ClassVar[bool] = True
 
     client: Aria2WebSocket
+    data: Dict[str, asyncio.Queue]
+    downloads: Dict[str, str]
 
     async def on_load(self) -> None:
+        self.data = {}
         self.client = await Aria2WebSocket.init(self)
 
     async def on_stop(self) -> None:
         await self.client.close()
 
-    async def get_file(self, gid: str) -> Dict[str, Any]:
+    async def onDownloadStart(self, gid: str) -> None:
+        self.log.info(f"Starting download: {gid: {gid}}.")
+
+    async def onDownloadComplete(self, gid: str):
+        meta = ""
+        if self.downloads.get(gid):
+            _, metadata = self.downloads[gid]
+            if metadata is True:
+                meta += " - MetaData"
+
+        self.log.info(f"Complete download: {gid: {gid}}{meta}.")
+
+    async def onDownloadError(self, gid: str) -> None:
+        res = await self.client.tellStatus(gid, ["errorMessage"])
+        self.log.warning(res["errorMessage"])
+
+    async def addDownload(self, uri: str) -> str:
+        gid = await self.client.addUri([uri])
+
+        self.data[gid] = asyncio.Queue(1)
+        fut, metadata = await asyncio.wait_for(self.data[gid].get(), 10)
+        if fut is not None and metadata is True:
+            self.downloads[gid] = (fut, metadata)
+            return fut
+
+        return gid
+
+    async def pauseDownload(self, gid: str) -> str:
+        return await self.client.pause(gid)
+
+    async def removeDownload(self, gid: str) -> str:
+        return await self.client.remove(gid)
+
+    async def getDownload(self, gid: str) -> Dict[str, Any]:
         res = await self.client.tellStatus(
             gid,
             [

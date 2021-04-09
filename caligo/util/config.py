b27df74007ed7fbe9445b7000a8f10c267be6c59
Adek Maulana <adek@techdro.id>
2021-04-09 19:59:26 +0700

    Config: logic fixes
    Change-Id: I204ff1725c03625e961a984e945a0eda35c86ad1

--- caligo/util/config.py
+++ caligo/util/config.py
@@ -6,6 +6,14 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 
+def _replace(key: str) -> None:
+    """ replace empty string '' to None """
+    if key == "":
+        return None
+
+    return key
+
+
 @dataclass
 class BotConfig:
     """
@@ -17,10 +25,10 @@ class BotConfig:
             load_dotenv("config.env")
 
         # Optional
-        path = self._replace(os.environ.get("DOWNLOAD_PATH"))
+        path = _replace(os.environ.get("DOWNLOAD_PATH"))
         self.downloadPath = Path(path) if path else Path.home() / "downloads"
 
-        self.token = self._replace(os.environ.get("BOT_TOKEN"))
+        self.token = _replace(os.environ.get("BOT_TOKEN"))
 
         # Core config
         self.api_id = int(os.environ.get("API_ID", 0))
@@ -33,25 +41,19 @@ class BotConfig:
             self.gdrive_secret = json.loads(os.environ.get("G_DRIVE_SECRET"))
         except (TypeError, json.decoder.JSONDecodeError):
             self.gdrive_secret = None
-        self.gdrive_folder_id = self._replace(
+        self.gdrive_folder_id = _replace(
             os.environ.get("G_DRIVE_FOLDER_ID"))
-        self.gdrive_index_link = self._replace(
+        self.gdrive_index_link = _replace(
             os.environ.get("G_DRIVE_INDEX_LINK"))
 
         # Checker
         self.secret = bool(os.environ.get("CONTAINER") == "True")
 
         # Github
-        self.github_repo = self._replace(os.environ.get("GITHUB_REPO")
-                                         ) or "adekmaulana/caligo"
-        self.github_token = self._replace(os.environ.get("GITHUB_TOKEN"))
+        self.github_repo = _replace(os.environ.get("GITHUB_REPO")
+                                    ) or "adekmaulana/caligo"
+        self.github_token = _replace(os.environ.get("GITHUB_TOKEN"))
 
         # Heroku
-        self.heroku_app_name = self._replace(os.environ.get("HEROKU_APP"))
-        self.heroku_api_key = self._replace(os.environ.get("HEROKU_API_KEY"))
-
-    def _replace(self, key: str) -> None:
-        if key == "":
-            return None
-        else:
-            return key
+        self.heroku_app_name = _replace(os.environ.get("HEROKU_APP"))
+        self.heroku_api_key = _replace(os.environ.get("HEROKU_API_KEY"))

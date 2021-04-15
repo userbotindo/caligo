e854f8e827f113d0db58416d91a7eb0f036b03ae
Adek Maulana <adek@techdro.id>
2021-04-15 18:28:01 +0700

    log: use colorlog when not running on container
    Change-Id: I8a87b7f9c6c0ed422111813c0c8074a3f44c0ecc

--- caligo/log.py
+++ caligo/log.py
@@ -1,27 +1,38 @@
 import logging
+import os
 from datetime import datetime
 from pathlib import Path
+
+import colorlog
+
+level = logging.INFO
 
 
 def setup_log() -> None:
     """Configures logging"""
     path = Path.home() / ".cache" / "caligo"
     path.mkdir(parents=True, exist_ok=True)
-
-    level = logging.INFO
+    # Check if running on container
+    container = bool(os.environ.get("CONTAINER") == "True")
 
     logging.root.setLevel(level)
 
-    logfile_format = "[ %(asctime)s : %(levelname)-7s ] %(name)-11s | %(message)s"
     logfile_name = f"caligo-{datetime.now().strftime('%Y-%m-%d')}.log"
     logfile = logging.FileHandler(f"{str(path)}/{logfile_name}")
-    formatter = logging.Formatter(logfile_format, datefmt="%H:%M:%S")
+    formatter = logging.Formatter("[ %(asctime)s : %(levelname)-7s ] "
+                                  "%(name)-11s | %(message)s",
+                                  datefmt="%H:%M:%S")
     logfile.setFormatter(formatter)
     logfile.setLevel(level)
 
-    stderr_format = "  %(levelname)-7s  |  %(name)-11s  |  %(message)s"
+    if container is True:
+        formatter = logging.Formatter(
+            "  %(levelname)-7s  |  %(name)-11s  |  %(message)s")
+    else:
+        formatter = colorlog.ColoredFormatter(
+            "  %(log_color)s%(levelname)-7s%(reset)s  |  "
+            "%(name)-11s  |  %(log_color)s%(message)s%(reset)s")
     stream = logging.StreamHandler()
-    formatter = logging.Formatter(stderr_format)
     stream.setLevel(level)
     stream.setFormatter(formatter)
 

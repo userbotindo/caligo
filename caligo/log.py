4d59acabe94c7607d27c9e1ea58161ffd892d974
Adek Maulana <adek@techdro.id>
2021-04-19 21:59:26 +0700

    log: remove logging file
    Change-Id: I22f559955d4dc04dcebf06ec90c03ed4561514ed

--- caligo/log.py
+++ caligo/log.py
@@ -1,7 +1,5 @@
 import logging
 import os
-from datetime import datetime
-from pathlib import Path
 
 import colorlog
 
@@ -10,20 +8,9 @@ level = logging.INFO
 
 def setup_log() -> None:
     """Configures logging"""
-    path = Path.home() / ".cache" / "caligo"
-    path.mkdir(parents=True, exist_ok=True)
     # Check if running on container
     container = bool(os.environ.get("CONTAINER") == "True")
-
     logging.root.setLevel(level)
-
-    logfile_name = f"caligo-{datetime.now().strftime('%Y-%m-%d')}.log"
-    logfile = logging.FileHandler(f"{str(path)}/{logfile_name}")
-    formatter = logging.Formatter("[ %(asctime)s : %(levelname)-7s ] "
-                                  "%(name)-11s | %(message)s",
-                                  datefmt="%H:%M:%S")
-    logfile.setFormatter(formatter)
-    logfile.setLevel(level)
 
     if container is True:
         formatter = logging.Formatter(
@@ -38,7 +25,6 @@ def setup_log() -> None:
 
     root = logging.getLogger()
     root.setLevel(level)
-    root.addHandler(logfile)
     root.addHandler(stream)
 
     logging.getLogger("pyrogram").setLevel(logging.ERROR)

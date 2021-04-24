3322605b52fae43851a6c5697cfc95b6c757fec2
Restyled.io <commits@restyled.io>
2021-04-24 09:00:05 +0000

    Restyled by yapf

--- caligo/modules/purge.py
+++ caligo/modules/purge.py
@@ -11,21 +11,23 @@ from .. import command, module
 class PurgeModule(module.Module):
     name: ClassVar[str] = "Purge"
 
-    @command.desc(
-        "reply to a message, mark as start until your purge command. "
-    )
+    @command.desc("reply to a message, mark as start until your purge command. "
+                 )
     @command.usage("purge", reply=True)
     async def cmd_purge(self, ctx: command.Context):
         """ This function need permission to delete messages. """
         if ctx.msg.chat.type in ["group", "supergroup"]:
-            perm = (await ctx.bot.client.get_chat_member(ctx.msg.chat.id, "me")).can_delete_messages
+            perm = (await
+                    ctx.bot.client.get_chat_member(ctx.msg.chat.id,
+                                                   "me")).can_delete_messages
             if perm is not True:
                 return "**You can't delete message in this chat..**"
         await ctx.respond("Purging...")
         msg_ids = []
         purging = 0
         t_s = datetime.now()
-        for msg_id in range(ctx.msg.reply_to_message.message_id, ctx.msg.message_id):
+        for msg_id in range(ctx.msg.reply_to_message.message_id,
+                            ctx.msg.message_id):
             msg_ids.append(msg_id)
             if len(msg_ids) == 100:
                 await ctx.bot.client.delete_messages(
@@ -45,11 +47,14 @@ class PurgeModule(module.Module):
             purging += len(msg_ids)
         t_e = datetime.now()
         run_time = (t_e - t_s).seconds
-        await ctx.respond("`Purged {} messages in {} second...`".format(purging, run_time))
+        await ctx.respond("`Purged {} messages in {} second...`".format(
+            purging, run_time))
         await asyncio.sleep(5)
         await ctx.msg.delete()
 
-    @command.desc("This module, can delete your message, just reply message you wanna deleted.")
+    @command.desc(
+        "This module, can delete your message, just reply message you wanna deleted."
+    )
     @command.usage("del", reply=True)
     async def cmd_del(self, ctx: command.Context):
         """ reply to message as target, this function will delete that. """
